815bf58e2bcee2d821a98f441631aa9f6c270808
ramanamuttana <ramanamuttana@gmail.com>
2023-07-10 17:50:46 +0200

    Added Rest and extra features on Jetty and vaadin

--- src/main/java/com/vaadin/starter/skeleton/Main.java
+++ src/main/java/com/vaadin/starter/skeleton/Main.java
@@ -3,12 +3,130 @@ package com.vaadin.starter.skeleton;
 import com.github.mvysny.vaadinboot.VaadinBoot;
 import org.jetbrains.annotations.NotNull;
 
+
+import com.vaadin.flow.server.VaadinServlet;
+import com.vaadin.flow.server.startup.ServletContextListeners;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.fkie.rm.consumer.controller.ConsumerRequestResourceController;
+import org.jetbrains.annotations.NotNull;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Run {@link #main(String[])} to launch your app in Embedded Jetty.
  * @author mavi
  */
+//public final class Main {
+//    public static void main(@NotNull String[] args) throws Exception {
+//        new VaadinBoot().withArgs(args).run();
+//    }
+//}
+
+
 public final class Main {
+
+    static ServletContextHandler xmlResourceContext;
+    private static Server server;
+
     public static void main(@NotNull String[] args) throws Exception {
-        new VaadinBoot().withArgs(args).run();
+        start(args);
+        server.join();
+    }
+
+    public static void start(@NotNull String[] args) throws Exception {
+        Config config = new Config();
+
+        // change this to e.g. /foo to host your app on a different context root
+        final String contextRoot = "/app";
+
+        // detect&enable production mode
+        if (isProductionMode()) {
+            // fixes https://github.com/mvysny/vaadin14-embedded-jetty/issues/1
+            System.out.println("Production mode detected, enforcing");
+            System.setProperty("vaadin.productionMode", "true");
+        }
+
+        final WebAppContext context = new WebAppContext();
+        context.setBaseResource(findWebRoot());
+        context.setContextPath(contextRoot);
+        context.addServlet(VaadinServlet.class, "/*");
+        context.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*\\.jar|.*/classes/.*");
+        context.setConfigurationDiscovered(true);
+        context.getServletContext().setExtendedListenerTypes(true);
+        context.addEventListener(new ServletContextListeners());
+
+        int port = config.getPortNumber(); // port number
+
+        if (args.length >= 1) {
+            port = Integer.parseInt(args[0]);
+        }
+        server = new Server(port);
+
+        // restServlet();
+        // HandlerList handlers = new HandlerList();
+        // handlers.setHandlers(new Handler[] { context,xmlResourceContext });
+
+        // server.setHandler(handlers);
+
+        server.setHandler(context);
+        server.start();
+
+        System.out.println("\n\n=================================================\n\n" + "Please open http://localhost:"
+                + port + contextRoot + " in your browser\n\n"
+                + "If you see the 'Unable to determine mode of operation' exception, just kill me and run `./gradlew vaadinPrepareFrontend`\n\n"
+                + "=================================================\n\n");
+    }
+
+    public static void stop() throws Exception {
+        server.stop();
+        server = null;
+    }
+
+    private static boolean isProductionMode() {
+        final String probe = "META-INF/maven/com.vaadin/flow-server-production-mode/pom.xml";
+        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        return classLoader.getResource(probe) != null;
+    }
+
+    @NotNull
+    private static Resource findWebRoot() throws MalformedURLException {
+        // don't look up directory as a resource, it's unreliable:
+        // https://github.com/eclipse/jetty.project/issues/4173#issuecomment-539769734
+        // instead we'll look up the /webapp/ROOT and retrieve the parent folder from
+        // that.
+        final URL f = Main.class.getResource("/webapp/ROOT");
+        if (f == null) {
+            throw new IllegalStateException(
+                    "Invalid state: the resource /webapp/ROOT doesn't exist, has webapp been packaged in as a resource?");
+        }
+        final String url = f.toString();
+        if (!url.endsWith("/ROOT")) {
+            throw new RuntimeException("Parameter url: invalid value " + url + ": doesn't end with /ROOT");
+        }
+        System.err.println("/webapp/ROOT is " + f);
+
+        // Resolve file to directory
+        URL webRoot = new URL(url.substring(0, url.length() - 5));
+        System.err.println("WebRoot is " + webRoot);
+        return Resource.newResource(webRoot);
+    }
+
+
+    public static void restServlet() {
+        xmlResourceContext = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+        xmlResourceContext.setContextPath("/api");
+        // xmlResourceContext.addServlet(ConsumerRequestResourceController.class, );
+
+        ServletHolder servlet = xmlResourceContext.addServlet("org.glassfish.jersey.servlet.ServletContainer", "/*");
+        servlet.setInitOrder(1);
+        servlet.setInitParameter("jersey.config.server.provider.classnames",
+                ConsumerRequestResourceController.class.getCanonicalName());
+
     }
 }
+
